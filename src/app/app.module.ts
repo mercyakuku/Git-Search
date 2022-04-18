bc0d49804c81429f802032ef111ecf1d4df79ded
mercyakuku <mercyakuku24@gmail.com>
2022-04-18 18:15:24 +0300

    Created Search-Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -7,7 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+    //users:User;
+
+    profile: any;
+    repos: any;
+    userName: string;
+
+  constructor() { 
+    // private userService: UserService, private repositoryService: RepositoryService
+
+    // ) {
+    //   this.userService.getUsers().subscribe(profile => {
+    //     console.log(profile);
+    //     this.profile = profile;
+    //   });
+  
+    //   this.userService.getRepos().subscribe(repos => {
+    //     console.log(repos);
+    //     this.repos = repos;
+    //   })
+  
+    // }
+  
+  
+    // findUser() {
+    //   this.userService.updateUser(this.userName);
+    //   this.userService.getUserInfo()
+    //   this.profile = this.userService.profile;
+  
+    //   this.repositoryService.updateUser(this.userName);
+    //   this.repositoryService.getRepoInformation()
+    //   this.repos = this.repositoryService.repos;
+    // }
+  
 
   ngOnInit(): void {
   }
