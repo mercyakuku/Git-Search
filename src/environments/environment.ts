5b508994d1ce8aefa47f721db617990b185c1d7e
mercyakuku <mercyakuku24@gmail.com>
2022-04-22 19:15:45 +0300

    Added apiUrl, clientId and clientSecret

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: "https://github.com/settings/tokens/",
+  clientid: '64d6ff1778b02ddc99d8',
+  clientsecret: '1b3d1f198340208ae963714e0b5163de8ca5a291',
 };
 
 /*
