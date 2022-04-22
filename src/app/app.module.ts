7cba61c5a6d9c1cf79daad84f5648f2569a28dbe
mercyakuku <mercyakuku24@gmail.com>
2022-04-22 14:52:02 +0300

    Delete all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SearchFormComponent } from './search-form/search-form.component';
-import { UserRepositoriesComponent } from './user-repositories/user-repositories.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    SearchFormComponent,
-    UserRepositoriesComponent,
+    
   ],
   imports: [
     BrowserModule,
