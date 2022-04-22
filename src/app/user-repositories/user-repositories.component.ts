8f2b5d9ba22743b012b55101b52bef45a1413bfe
mercyakuku <mercyakuku24@gmail.com>
2022-04-22 19:28:34 +0300

    defined user's profile and imported UserRepositoriesService

--- src/app/user-repositories/user-repositories.component.ts
+++ src/app/user-repositories/user-repositories.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
+import { UserRepositoriesService } from '../services/user-repositories.service';   
 
 @Component({
   selector: 'app-user-repositories',
@@ -6,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-repositories.component.css']
 })
 export class UserRepositoriesComponent implements OnInit {
+  profile: any = { login: '',
+    company: '',
+    location: '',
+    avatar_url: '',
+    public_repos: '',
+    public_gist: '',
+    followers: '',
+    following: '',
+    email: '',
+    bio: '',
+    created_at: ''};
+    repos: any;
+    username!: string;
 
-  constructor() { }
 
+  constructor(private service: UserRepositoriesService) {
+    this.service.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile = profile;
+    });
+    this.service.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
+
+
+  }
+
+  findProfile() {
+    this.service.updateProfile(this.username);
+    this.service.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile = profile;
+    });
+    this.service.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
+  }
+  
   ngOnInit(): void {
   }
 
